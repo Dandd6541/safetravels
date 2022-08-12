505a46a1112f79ad358e1373577820a8db6cabee
abpandanguyen <abpandanguyen@gmail.com>
2022-08-12 16:12:46 -0700

    the first step

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Trip(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    county = models.CharField(max_length=50)
+    stayLength = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
