3b24751e0d650fdde337fd731df8277095081001
abpandanguyen <abpandanguyen@gmail.com>
2022-08-15 10:14:38 -0700

    details

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('trips/', views.trips_index, name='index'),
+    path('trips/<int:trip_id>', views.trips_detail, name='detail'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
 
