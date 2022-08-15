9ed998a457d1859ae0e88dbba9aecdb70a743605
abpandanguyen <abpandanguyen@gmail.com>
2022-08-15 09:14:59 -0700

    date

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
+from django.urls import reverse
 
 # Create your models here.
 class Trip(models.Model):
@@ -7,6 +9,7 @@ class Trip(models.Model):
     city = models.CharField(max_length=50)
     county = models.CharField(max_length=50)
     stayLength = models.IntegerField()
+    date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
