22b9a354fa06f1f0c94c3620fa2074662128c4eb
abpandanguyen <abpandanguyen@gmail.com>
2022-08-17 16:53:44 -0700

    trip user centric

--- main_app/views.py
+++ main_app/views.py
@@ -71,7 +71,7 @@ def covid_query(request):
 
 @login_required
 def trips_index(request):
-    trips = Trip.objects.all()
+    trips = Trip.objects.filter(user=request.user)
     return render(request, 'trips/index.html', { 'trips': trips})
 
 @login_required  
